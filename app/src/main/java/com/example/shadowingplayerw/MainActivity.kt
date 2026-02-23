27925d5ae513f179283d84b44adf5ab9c5a113b4
walid sobhi <alwalidalsobhi1@gmail.com>
2026-02-23 20:54:07 +0200

    مرحبا وليد

--- app/src/main/java/com/example/shadowingplayerw/MainActivity.kt
+++ app/src/main/java/com/example/shadowingplayerw/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ShadowingPlayerWTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "وليد",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "مرحبا $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ShadowingPlayerWTheme {
-        Greeting("Android")
+        Greeting("وليد")
     }
 }
